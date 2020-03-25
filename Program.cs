341e7598e9b88464713bbfbf81dd621af72e3298
kevin <kevingleijm@hotmail.com>
2020-03-25 16:46:25 +0100

    skeleton implemented

--- Program.cs
+++ Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace project_bioscooop
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static int permission = 0;
+        
+        private Dictionary<int, Movie> movieList = new Dictionary<int,Movie>();
+        private Dictionary<int, Ticket> ticketList = new Dictionary<int,Ticket>();
+        private Dictionary<int, Account> accountList = new Dictionary<int,Account>();
+        private Dictionary<int, Theater> theaterList = new Dictionary<int,Theater>();
+
+        
+        
+        
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("corona!");
         }
+        
+        public class Account
+        {
+            public readonly int permission;
+            public readonly string name;
+            public readonly int accountId;
+            public readonly string email;
+                    
+                    
+        
+            public Account(string inp_name, int inp_accountId, int inp_permission, string inp_email)
+            {
+                permission = inp_permission;
+                email = inp_email;
+                accountId = inp_accountId;
+                name = inp_name;
+            }
+                    
+        
+        }
+        
+        public static class Menu
+        {
+            static List<FoodItem> foodItems = new List<FoodItem>();
+            
+
+            public class FoodItem
+            {
+                private String[] ingredients;
+                private int price;
+            }
+        }
+
+        public class Ticket
+        {
+            public readonly Theater Theater;
+            public readonly Movie movie;
+            public int price;
+        }
+
+        public class Movie
+        {
+            private DateTime time;
+            private int price;
+
+        }
+
+        public class Theater
+        {
+            public readonly int theaterId;
+            private int[] prices;
+        }
+
+
+
     }
 }
