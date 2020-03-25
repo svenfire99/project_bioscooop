0d2a6a49e0658c88923e304b669a8dc46f9d3d37
kevin <kevingleijm@hotmail.com>
2020-03-25 22:03:13 +0100

    skeleton implemented

--- Program.cs
+++ Program.cs
@@ -9,31 +9,133 @@ namespace project_bioscooop
         
         private Dictionary<int, Movie> movieList = new Dictionary<int,Movie>();
         private Dictionary<int, Ticket> ticketList = new Dictionary<int,Ticket>();
-        private Dictionary<int, Account> accountList = new Dictionary<int,Account>();
+        private Dictionary<string, Account> accountList = new Dictionary<string,Account>();
         private Dictionary<int, Theater> theaterList = new Dictionary<int,Theater>();
 
+        private const int STATE_EXIT = -1;
+        private const int STATE_MAIN = 0;
+        private const int STATE_CREATE_ACCOUNT = 1;
         
-        
-        
+        private static int currentState = 0;
+
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("corona!");
+            //main loop
+            while (currentState != STATE_EXIT) {
+                switch (currentState)
+                {
+                    case STATE_MAIN: stateMain(); break;
+
+                }
+            }
         }
         
+        //main methods
+        public static void stateMain()
+        {
+            //intro
+            Console.WriteLine("Welcome to Cinema, What would you like to do?\n");
+            Console.WriteLine("[L]ogin");
+            Console.WriteLine("[C]reate account");
+            Console.WriteLine("[E]xit\n");
+
+            String input = Console.ReadLine();
+            
+            //Choice menu
+            switch (input)
+            {
+                case "L": /*shit*/ ; break;
+                case "C": stateCreateAccount(); break;
+                case "E": currentState = STATE_EXIT ; break; 
+                default: Console.WriteLine("\nunknown command"); stateMain(); break;
+            }
+            
+            
+            
+
+        }
+        
+        public static void stateCreateAccount()
+        {
+            string name;
+            string password;
+            string email;
+            int age;
+
+            bool creating = true;
+            
+            
+            //TODO add checks
+            //intro
+            Console.WriteLine("Cool! thanks for making an account with us :)\n");
+
+            while (creating)
+            {
+             Console.WriteLine("Lets begin with your name!\n");
+             name = Console.ReadLine();
+             Console.WriteLine("Now an easy to remember, hard to guess password:\n");
+             password = Console.ReadLine();
+
+             
+             //check email
+             email = "default";
+             bool check = true;
+             while(!email.Contains("@"))
+             {
+                string output = !email.Contains("@") || email.Equals("default")?  "Great! on which email-adress can we reach you?\n" : "that's not a  real email! :(\n";
+                Console.WriteLine(output);
+                email = Console.ReadLine();
+             }
+             
+             //check age
+             Console.WriteLine("Finally, and don't lie, How old are you?\n");
+             if (int.TryParse(Console.ReadLine(), out int inAge))
+             {
+                 age = inAge;
+             }
+             else
+             {
+                 
+             }
+
+
+            }
+            
+            
+            
+            
+
+        }
+        
+        
+        
+        
+        
+        
+        
+        //classes
         public class Account
         {
             public readonly int permission;
             public readonly string name;
             public readonly int accountId;
             public readonly string email;
-                    
-                    
-        
-            public Account(string inp_name, int inp_accountId, int inp_permission, string inp_email)
+            
+            private static int AccountIDCounter = 0;
+
+            private static int getAccountID()
+            {
+                AccountIDCounter++;
+                return AccountIDCounter;
+            }        
+            
+            
+            public Account(string inp_name, int inp_permission, string inp_email)
             {
                 permission = inp_permission;
                 email = inp_email;
-                accountId = inp_accountId;
+                accountId = getAccountID();
                 name = inp_name;
             }
                     
@@ -54,6 +156,7 @@ namespace project_bioscooop
 
         public class Ticket
         {
+            public readonly int ticketID;
             public readonly Theater Theater;
             public readonly Movie movie;
             public int price;
