18616a9e7a32bf0fae728fd60f6abd664c503c8a
Ahmet <ahmedozkan_1@hotmail.com>
2020-03-30 12:31:46 +0200

    Initial branch commit

--- Program.cs
+++ Program.cs
@@ -6,29 +6,26 @@ namespace project_bioscooop
     internal class Program
     {
         private static int permission = 0;
-        
-        private Dictionary<int, Movie> movieList = new Dictionary<int,Movie>();
-        private Dictionary<int, Ticket> ticketList = new Dictionary<int,Ticket>();
-        private Dictionary<int, Account> accountList = new Dictionary<int,Account>();
-        private Dictionary<int, Theater> theaterList = new Dictionary<int,Theater>();
 
-        
-        
-        
+        private Dictionary<int, Movie> movieList = new Dictionary<int, Movie>();
+        private Dictionary<int, Ticket> ticketList = new Dictionary<int, Ticket>();
+        private Dictionary<int, Account> accountList = new Dictionary<int, Account>();
+        private Dictionary<int, Theater> theaterList = new Dictionary<int, Theater>();
+
+
         public static void Main(string[] args)
         {
             Console.WriteLine("corona!");
         }
-        
+
         public class Account
         {
             public readonly int permission;
             public readonly string name;
             public readonly int accountId;
             public readonly string email;
-                    
-                    
-        
+
+
             public Account(string inp_name, int inp_accountId, int inp_permission, string inp_email)
             {
                 permission = inp_permission;
@@ -36,14 +33,12 @@ namespace project_bioscooop
                 accountId = inp_accountId;
                 name = inp_name;
             }
-                    
-        
         }
-        
+
         public static class Menu
         {
             static List<FoodItem> foodItems = new List<FoodItem>();
-            
+
 
             public class FoodItem
             {
@@ -63,7 +58,6 @@ namespace project_bioscooop
         {
             private DateTime time;
             private int price;
-
         }
 
         public class Theater
@@ -71,8 +65,5 @@ namespace project_bioscooop
             public readonly int theaterId;
             private int[] prices;
         }
-
-
-
     }
 }
